e60d043ff8bd2a3e735815865a4f101bc427ba88
Piotr Kniaź <pio.kniaz@gmail.com>
2023-07-06 19:53:01 +0200

    grafbase add

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>123</h1>
+    <main className="flex-start flex-col px-8 py-6">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>Load more</h1>
     </main>
   )
 }
